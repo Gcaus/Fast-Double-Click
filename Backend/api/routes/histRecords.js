f9a54ec7db1a3c6ce33cf4fe3f4e9034a8e62bbf
gcaus13@gmail.com <gcaus13@gmail.com>
2023-04-27 09:08:35 -0300

    rota histRecords

---
+++ Backend/api/routes/histRecords.js
@@ -0,0 +1,38 @@
+import { Router } from 'express'
+import fs from 'fs'
+
+const router = Router();
+
+const FILE = 'records.json'
+
+
+router.get('/', (_req, res) => { 
+   fs.readFile(FILE, 'utf8', (err, data) => { 
+     try { 
+       const records = JSON.parse(data); 
+       res.status(200).send(records); 
+     } catch (err) { 
+       console.error(err); 
+       res.status(500).send('Error reading log file'); 
+     } 
+   }); 
+ });
+ 
+ 
+ router.delete('/', (_req, res) => { 
+   fs.readFile(FILE, () => { 
+     const newRecord = []; 
+  
+     fs.writeFile(FILE, JSON.stringify(newRecord), err => { 
+       try { 
+         res.status(204).send('Records deleted successfully'); 
+       } catch (err) { 
+         console.error(err); 
+         res.status(500).send('Error delete file'); 
+       } 
+     }); 
+   }); 
+ });
+ 
+ 
+ export daafult histRecords
